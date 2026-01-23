6eadea89e41bd2108559946adf47c99e90d4f7be
jacopone <828254+jacopone@users.noreply.github.com>
2026-01-23 09:12:30 +0000

    chore: update cursor to 2.4.21

--- package.nix
+++ package.nix
@@ -32,12 +32,12 @@
 
 let
   pname = "cursor";
-  version = "2.3.41";
+  version = "2.4.21";
 
   sources = {
     x86_64-linux = fetchurl {
-      url = "https://downloads.cursor.com/production/2ca326e0d1ce10956aea33d54c0e2d8c13c58a32/linux/x64/Cursor-${version}-x86_64.AppImage";
-      hash = "sha256-ItUgknMzSDeXxN3Yi/pz2wZoz7vVVqx9nGXuGmbHbXc=";
+      url = "https://downloads.cursor.com/production/dc8361355d709f306d5159635a677a571b277bcc/linux/x64/Cursor-${version}-x86_64.AppImage";
+      hash = "sha256-OOjANfVHMlRN1uWq2jNmK/RqI4Q5NTlN/19Nl2jWiKI=";
     };
     aarch64-linux = fetchurl {
       url = "https://downloads.cursor.com/production/b9e5948c1ad20443a5cecba6b84a3c9b99d62582/linux/arm64/Cursor-${version}-aarch64.AppImage";
